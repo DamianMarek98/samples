eb277b00eb8fcbe0c0d7f64c50bea34d24da4f86
damianmarek <damianmarek@carfax.com>
2025-05-20 19:18:18 +0200

    Running app with pg store

--- mcp/src/main/java/com/example/mcp/McpApplication.java
+++ mcp/src/main/java/com/example/mcp/McpApplication.java
@@ -1,13 +1,52 @@
 package com.example.mcp;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.model.bedrock.titan.autoconfigure.BedrockTitanEmbeddingAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		BedrockTitanEmbeddingAutoConfiguration.class
+})
 public class McpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(McpApplication.class, args);
 	}
 
+	@Bean
+	ChatClient singularity(ChatClient.Builder builder) {
+		return builder.build();
+	}
 }
+
+@Controller
+@ResponseBody
+class AdoptionsAssistantController {
+	private final ChatClient singularity;
+
+	AdoptionsAssistantController(ChatClient singularity) {
+		this.singularity = singularity;
+	}
+
+	@GetMapping("/{user}/inquire")
+	String inquire(@RequestParam String question, @PathVariable String user) {
+		return singularity.prompt()
+				.user(question)
+				.call()
+				.content();
+	}
+}
+
+
+interface DogRepository extends ListCrudRepository<Dog, Integer> {}
+
+record Dog (@Id int id, String name, String owner, String description) {}
